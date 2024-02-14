a7cbde8948b932f7ebefc972ea8a4841c09930a9
Loveisblind0 <125294644+Loveisblind0@users.noreply.github.com>
2024-02-14 21:57:53 +0530

    cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='https://t.me/ToonGru_bot'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/misterrnova/Vega-File-Store-Bot'>Click here</a>\n○ Channel : @TheToonGru\n○ Support Bot : @ToonGru_bot</b>",
+            text = f"<b>○ Creator : <a href='https://t.me/ToonGru_bot'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/'>Paid</a>\n○ Channel : @TheToonGru\n○ Support Bot : @ToonGru_bot</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
